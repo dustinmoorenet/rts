64f160760ecbac12c47ea5ba29914d3c03d0cfc1
Dustin Moore <dustin@ordoro.com>
2016-05-05 22:19:43 -0500

    Added SVG

---
+++ src/js/Map/component.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Map() {
+    return (
+        <svg width="400" height="400">
+            <circle cx="60" cy="60" r="50" />
+        </svg>
+    );
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,9 @@
-console.log('we need some source files');
+import React from 'react';
+import {render} from 'react-dom';
+
+import Map from './Map/component.js';
+
+render(
+    <Map />,
+    document.body.querySelector('.app-view')
+);
